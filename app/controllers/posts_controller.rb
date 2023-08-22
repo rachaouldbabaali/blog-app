17fe15ba3f54d874d5ecec4eef4962e32c450182
Racha OULDBABAALI <rachaould@gmail.com>
2023-08-22 15:24:51 +0100

    Fix rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,11 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
   def new
     @post = Post.new
   end
+
   def show
     @post = Post.find(params[:id])
   end
