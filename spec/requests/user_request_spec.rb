957e6c69f55c4b3efad2ef5fedda2326a4c5ae32
Racha OULDBABAALI <rachaould@gmail.com>
2023-08-23 16:54:14 +0100

    Modify user request spec

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,7 +1,18 @@
-describe "GET /show" do
-  it "renders a successful response" do
-      user =User.create! Valid_attributes
-      get user_url(user)
-      expect(response).to be_successful
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    it 'responds with HTTP success' do
+      get users_path
+      expect(response).to have_http_status(:success)
+    end
+    it 'renders the index template' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+    it 'includes correct placeholder text in the response body' do
+      get users_path
+      expect(response.body).to include('Hello! This a list of users! index')
+    end
   end
 end
