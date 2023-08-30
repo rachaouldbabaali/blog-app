6ccd0b4939c15bdb22323d92c289e585cd6372ca
Racha OULDBABAALI <rachaould@gmail.com>
2023-08-30 18:55:06 +0100

    Update ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,21 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     # Define abilities for the user here. For example:
     #
-    #   return unless user.present?
-    #   can :read, :all
+    return unless user.present?
+
+    can :read, :all
+    can :create, [Comment, Post]
+
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :destroy, Comment, author: user
+      can :destroy, Post, author: user
+    end
+
     #   return unless user.admin?
     #   can :manage, :all
     #
