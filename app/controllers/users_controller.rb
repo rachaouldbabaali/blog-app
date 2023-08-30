54b2d92c0cf37cb0dab03dba0d393f5c821844b3
Racha OULDBABAALI <rachaould@gmail.com>
2023-08-30 15:23:27 +0100

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @all_users = User.all
   end
 
   def show
-    @user = User.includes(posts: [:user, { comments: :user }]).find(params[:id])
-    @most_recent_posts = @user.most_recent_posts
-    @user_posts = @user.posts
+    if params[:id] == 'sign_out'
+      sign_out_and_redirect
+    else
+      @user = User.includes(posts: [:user, { comments: :user }]).find(params[:id])
+      @most_recent_posts = @user.most_recent_posts
+      @user_posts = @user.posts
+      redirect_to users_path, alert: 'User not found' unless @user
+    end
+
   end
+
+  def sign_out_and_redirect
+    sign_out current_user
+    redirect_to new_user_session_path
+  end
+
 end
