ce6ad521506a49ec55022dea5276e39921929ee5
Racha OULDBABAALI <rachaould@gmail.com>
2023-08-31 16:38:40 +0100

    Update controllers

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,18 @@
 class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @like = @post.likes.new(user: current_user)
+    @like = @post.likes.new(author_id: current_user.id) # Update this line
 
     if @like.save
       redirect_to user_post_path(current_user, @post), notice: 'Like was successfully added.'
     else
       redirect_to user_post_path(current_user, @post), alert: 'Failed to add like.'
     end
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    @like.destroy
+    redirect_to user_post_path(params[:user_id], params[:post_id]), notice: 'Like was successfully removed.'
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 require 'will_paginate/array'
 
 class PostsController < ApplicationController
+  load_and_authorize_resource
   before_action :set_user, only: %i[index show new create]
 
   def index
@@ -10,10 +11,16 @@ class PostsController < ApplicationController
   end
 
   def show
-    set_user
+    @post = Post.find(params[:id])
+    @user = User.find(@post.author_id)
+    render 'show'
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
-    @comments = @post.comments.includes(:user)
-    @comment = @post.comments.build
+    @post.destroy
+    redirect_to user_posts_path(@user)
   end
 
   def new
@@ -43,5 +50,13 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :text)
+  end
+
+  def authorize_user
+    @post = Post.find(params[:id])
+    unless @post.user == current_user
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to root_path
+    end
   end
 end
