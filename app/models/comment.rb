bce3aa839334ed9291a1d15e8c29a8bd45057b24
Racha OULDBABAALI <rachaould@gmail.com>
2023-08-31 16:39:18 +0100

    update models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,20 @@
 class Comment < ApplicationRecord
   belongs_to :post, foreign_key: 'post_id'
-  belongs_to :user, foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   after_save :comments_update
+  after_destroy :decrement_post_comment_counter
+
+  validates :text, presence: true, length: { maximum: 1000 }
+
+  def decrement_post_comment_counter
+    post.decrement!(:comments_counter)
+  end
 
   private
 
   def comments_update
     post.increment!(:comments_counter)
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   #      :recoverable, :rememberable, :validatable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
